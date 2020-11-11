ba93874e4d46dbc3e2bccd63574a4b9107aa4226
睿 蔡 <RayChromium@outlook.com>
2020-11-11 11:48:37 +0800

    vs stuff

---
+++ Date-A-Match/src/Person.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+#include <cassert>
+#include <iostream>
+
+enum class Gender
+{
+	Male,
+	Female,
+	Count
+};
+
+class Person
+{
+public:
+	Person(const std::string& name,
+		   const std::string& qq,
+		   const std::string& phoneNumber,
+		   const std::vector<int>& srcAttr,
+		   int genderVal)
+		:
+		name(name),
+		qq(qq),
+		phoneNumber(phoneNumber),
+		attributes(srcAttr),
+		gender(genderVal == 1 ? Gender::Male : Gender::Female)
+	{
+	}
+	void Print()
+	{
+		std::cout << "姓名：" << name << " QQ: " << qq << " phone: " << phoneNumber << " Attrbutes: ";
+		std::cout << " 性别： " << (gender == Gender::Male ? "男 " : "女 ");
+		for (auto i : attributes)
+		{
+			std::cout << i << " ";
+		}
+		std::cout<<std::endl;
+	}
+private:
+	std::string name;
+	std::string qq;
+	std::string phoneNumber;
+	std::vector<int> attributes;
+	Gender gender;
+};
